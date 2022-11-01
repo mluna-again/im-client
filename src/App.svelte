3e20f3ad8426616d5d50e88ea516de8e99ab415b
Manuel Luna <manuellunada@gmail.com>
2022-11-01 13:03:34 -0600

    hi

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Button from './lib/Button.svelte'
+</script>
+
+<main>
+  <h1>Welcome to IM!</h1>
+
+  <div class="btn">
+    <Button class="btn" message={"Create Room"} />
+  </div>
+
+  <div class="btn">
+    <Button class="btn" message={"Access Existing Room"} />
+  </div>
+</main>
+
+<style>
+  main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    padding-top: 3rem;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    font-size: 4rem;
+    margin-bottom: 3rem;
+  }
+
+  .btn {
+    margin: 10px 0;
+  }
+</style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let message;
+  export let reverse = false;
+</script>
+
+<button>
+  {#each message.split("") as letter}
+    <span>{letter}</span>
+  {/each}
+</button>
+
+<style>
+  button span {
+    display: inline-block;
+    margin: 0 3px;
+    transition: all 0.1s ease;
+  }
+
+  button span:nth-child(even) {
+    transform: translateY(-3px);
+  }
+
+  button span:nth-child(odd) {
+    transform: translateY(3px);
+  }
+
+  button:hover span:nth-child(even) {
+    transform: translateY(3px) rotate(10deg) scale(1.1);
+  }
+
+  button:hover span:nth-child(odd) {
+    transform: translateY(-3px) rotate(-10deg) scale(0.9);
+  }
+
+  button {
+    background-color: black;
+    border: none;
+    padding: 1rem;
+    font-family: 'Earwig Factory';
+    font-size: 2rem;
+    transform: skew(3deg, 3deg);
+    transition: all 0.1s ease;
+    cursor: pointer;
+  }
+
+  button:hover {
+    transform: skew(-3deg, -3deg);
+  }
+</style>
