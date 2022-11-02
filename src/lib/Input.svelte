bd2ff68b4124b5ed68f4d101efe3fbf8c0bf99c7
Manuel Luna <manuellunada@gmail.com>
2022-11-01 18:03:47 -0600

    form

---
+++ src/lib/Input.svelte
@@ -0,0 +1,52 @@
+<script>
+  export let reverse = false;
+</script>
+
+<div class="container" class:reverse>
+  <input type="text">
+</div>
+
+<style>
+  .container.reverse {
+    background-color: black;
+    transform: skew(-20deg);
+  }
+
+  .container.reverse input {
+    background-color: white;
+    transform: skew(20deg);
+    color: black;
+  }
+
+  .container {
+    margin: 10px 0;
+    background-color: white;
+    padding: 15px;
+    transform: skew(20deg);
+    transition: all 0.1s ease;
+  }
+
+  .container:hover {
+    transform: skew(20deg) rotate(2deg);
+  }
+
+  .container.reverse:hover {
+    transform: skew(-20deg) rotate(2deg);
+  }
+
+  input {
+    height: 35px;
+    width: min-content;
+    background-color: black;
+    border: none;
+    transform: skew(-20deg);
+    color: white;
+    text-align: center;
+    font-family: 'Poor Story', cursive;
+    font-size: 2rem;
+  }
+
+  input:focus {
+    outline: none;
+  }
+</style>
--- src/routes/SignIn.svelte
+++ src/routes/SignIn.svelte
@@ -1,23 +1,39 @@
 <script>
   import { fly } from 'svelte/transition';
   import GoBack from '../lib/GoBack.svelte';
+  import Input from '../lib/Input.svelte';
+  import Button from '../lib/Button.svelte';
 </script>
 
 <div class="container" in:fly="{{duration: 300, y: 200}}">
   <GoBack />
 
   <h1>Sign in</h1>
+
+  <Input />
+  <Input reverse />
+
+  <div class="btn">
+    <Button message="Create Account" onClick={() => alert("abueno")} />
+  </div>
 </div>
 
 <style>
   .container {
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
     position: relative;
   }
 
   h1 {
     font-size: 4rem;
+    margin-top: 2rem;
+    margin-bottom: 4rem;
+  }
+
+  .btn {
     margin-top: 2rem;
   }
 </style>
