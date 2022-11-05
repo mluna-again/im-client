b9285c15370019e7fce761f410de4a3db134bfcd
Manuel Luna <manuellunada@gmail.com>
2022-11-05 16:07:21 -0600

    no special characters on username

--- src/validators/username.js
+++ src/validators/username.js
@@ -1,6 +1,8 @@
 export const validateUsername = (username) => {
 	if (username.length < 4) return 'too short';
 	if (username.length > 15) return 'too long';
+	if (/[^a-zA-Z0-9_]/i.test(username))
+		return 'only letters, numbers and _ are allowed';
 
 	return null;
 };
